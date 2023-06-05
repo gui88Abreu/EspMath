a4f6559311d2618b052727ec848fb7d4c8bd28a5
gui88Abreu <guiii88@outlook.com>
2023-06-05 03:54:28 -0300

    make memCaps const

--- examples/test_bench/test_bench.ino
+++ examples/test_bench/test_bench.ino
@@ -2,7 +2,7 @@
 
 void setup(){
   vTaskDelay(pdMS_TO_TICKS(5000));
-  debug.print("Free size[bytes]: " + String(heap_caps_get_free_size(MALLOC_CAP_DEFAULT | MALLOC_CAP_8BIT)));
+  debug.print("Free size[bytes]: " + String(heap_caps_get_free_size(Array<>::memCaps())));
   debug.print("Testing floating-point arrays arithmetic...");
   test_ari<float,10>();
   debug.print("Succeeded!");
@@ -15,7 +15,7 @@ void setup(){
   debug.print("Testing integer 8 bits arrays arithmetic...");
   test_ari<int8_t,10>();
   debug.print("Succeeded!");
-  debug.print("Free size[bytes]: " + String(heap_caps_get_free_size(MALLOC_CAP_DEFAULT | MALLOC_CAP_8BIT)));
+  debug.print("Free size[bytes]: " + String(heap_caps_get_free_size(Array<>::memCaps())));
 }
 
 void loop(){}
