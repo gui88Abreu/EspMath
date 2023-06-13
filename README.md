96533b50bbbd2fff70f648ea63d395e6a75d8b92
gui88Abreu <guiii88@outlook.com>
2023-06-12 21:20:52 -0300

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EspMath
-Library that implements tools to perform optimized math operations for ESP32 devices
+
+This library is intended to provide useful tools to perform math operations such arrays multiplication, convolution, and etc, on ESP32 devices. Most of the operations uses **DSP** (Digital Signal Processing) instructions to accelerate the procedure.
+
+## Array
+
+The array class allows us to provide multiple features to perform the essential operations for an array type. For more information, please, read its documentation alongside the code at [Array](src/esp_array.h).
+
+## ANSI version
+
+An ANSI version of some operations is provided as well, so you can compare their performances. It can be compiled on any machine and it doesn't use any acceleration method or tool.
---
+++ src/ansi.h
@@ -0,0 +1,190 @@
+#ifndef _ANSI_VERSION_H
+#define _ANSI_VERSION_H
+
+#include <Arduino.h>
+
+namespace espmath{
+  using namespace espmath;
+
+  /**
+   * @brief Compare floats.
+   * 
+   * @param f1
+   * @param f2
+   * @param EPSILON tolerance
+   * @return true floats are equal
+   * @return false floats aren't equal
+   */
+  inline const bool eqFloats(const float f1, const float f2, const float EPSILON = 0.0001)
+  {
+   return (fabs(f1 - f2) <= EPSILON);
+  }
+
+  /**
+   * @brief dest[i] = src[i]*cnst;
+   * 
+   * @tparam T1 Type of the source array.
+   * @tparam T2 Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T1, typename T2>
+  inline void cpyArray(const T1* src, T2* dest, const size_t len, const T2 cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = (T2)(src[i]*cnst);
+  }
+
+  /**
+   * @brief dest[i] = cnst;
+   * 
+   * @tparam T Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T>
+  inline void cpyConst(T* dest, const size_t len, const T cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = (T)(cnst);
+  }
+
+  /**
+   * @brief dest[i] += src[i]*cnst;
+   * 
+   * @tparam T1 Type of the source array.
+   * @tparam T2 Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T1, typename T2>
+  inline void addArrayToArray(const T1* src, T2* dest, const size_t len, const T2 cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] += (T2)(src[i]*cnst);
+  }
+
+  /**
+   * @brief dest[i] += cnst;
+   * 
+   * @tparam T Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+
+  template<typename T>
+  inline void addConstToArray(T* src, T* dest, const size_t len, const T cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = src[i] + cnst;
+  }
+
+  /**
+   * @brief dest[i] -= src[i]*cnst;
+   * 
+   * @tparam T1 Type of the source array.
+   * @tparam T2 Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T1, typename T2>
+  inline void subArrayFromArray(const T1* src, T2* dest, const size_t len, const T2 cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] -= (T2)(src[i]*cnst);
+  }
+
+  /**
+   * @brief dest[i] -= cnst;
+   * 
+   * @tparam T Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T>
+  inline void subConstFromArray(T* src, T* dest, const size_t len, const T cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = src[i] - cnst;
+  }
+
+  /**
+   * @brief dest[i] *= src[i]*cnst;
+   * 
+   * @tparam T1 Type of the source array.
+   * @tparam T2 Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T1, typename T2>
+  inline void mulArrayByArray(const T1* src, T2* dest, const size_t len, const T2 cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] *= (T2)(src[i]*cnst);
+  }
+
+  /**
+   * @brief dest[i] *= cnst;
+   * 
+   * @tparam T Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T>
+  inline void mulConstByArray(T* src, T* dest, const size_t len, const T cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = src[i]*cnst;
+  }
+
+  /**
+   * @brief dest[i] /= src[i]*cnst;
+   * 
+   * @tparam T1 Type of the source array.
+   * @tparam T2 Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T1, typename T2>
+  inline void divArrayByArray(const T1* src, T2* dest, const size_t len, const T2 cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] /= (T2)(src[i]*cnst);
+  }
+
+  /**
+   * @brief dest[i] /= src[i]*cnst;
+   * 
+   * @tparam T Type of the destination array.
+   * @param src Source array.
+   * @param dest Destination array.
+   * @param len Quantity of elements to copy.
+   * @param cnst Constant to multiply the output.
+   */
+  template<typename T>
+  inline void divArrayByConst(T* src, T* dest, const size_t len, const T cnst = 1)
+  {
+    for(size_t i = 0; i < len; i++)
+      dest[i] = src[i] / cnst;
+  }
+}
+
+#endif
