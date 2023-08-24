a02d009a0f5b28ebd9cb9403e3af4f2645bdc425
guilhAbreu <guiii88@outlook.com>
2023-08-23 23:40:11 -0300

    add dsp code to convert float to fixed and vice-versa

--- src/esp_fixed_point.h
+++ src/esp_fixed_point.h
@@ -3,10 +3,13 @@
 
 #include <Arduino.h>
 #include "esp_ansi.h"
+#include "esp_opt.h"
+#include "dsp/fixed/converter.h"
+#include "dsp/mulc/dsps_mulc_esp.h"
+#include "dsp/divc/dsps_divc_esp.h"
 
-#define DEFAULT_FRAC 8
-#define FP_MUL(x, y, f) ((((long long)x) * (y)) >> (f))
-#define FP_DIV(x, y, f) ((((long long)x) << (f)) / (y))
+#define FP_MUL(x, y, f) ((((long)x) * (y)) >> (f))
+#define FP_DIV(x, y, f) ((((long)x) << (f)) / (y))
 
 namespace espmath{
   /**
@@ -17,23 +20,21 @@ namespace espmath{
   {
   public:
     int16_t data = 0;
-    uint8_t frac = DEFAULT_FRAC;
+    uint8_t frac = FRACTIONAL;
 
     // Constructors
-    FixedPoint():data(0),frac(DEFAULT_FRAC){}
-    FixedPoint(float value, uint8_t fracBits = DEFAULT_FRAC):frac(fracBits){data = float2fixed(value, fracBits);}
-    FixedPoint(const FixedPoint& other):FixedPoint(other.data, other.frac){}
-    FixedPoint(const FixedPoint&& other):FixedPoint(other.data, other.frac){}
+    FixedPoint(){}
+    FixedPoint(float value, uint8_t fracBits = FRACTIONAL){frac = fracBits, data = toFixed(value);}
+    FixedPoint(const FixedPoint& other):data(other.data),frac(other.frac){}
+    FixedPoint(const FixedPoint&& other):data(other.data),frac(other.frac){}
 
     // Cast Operator
-    operator float() const {return fixed2float(data, frac);} // float
+    operator float() const {return toFloat(data);}
 
     // Assign Operators
     void operator=(const FixedPoint& other){data = other.data, frac = other.frac;}
     void operator=(const FixedPoint&& other){data = other.data, frac = other.frac;}
-    void operator=(float value){data = float2fixed(value, frac);}
-    template<typename T>
-    void operator=(T value){data = float2fixed((float)value, frac);}
+    void operator=(float value){data = toFixed(value);}
 
     // Comparison Operators
     bool operator==(FixedPoint& other){return data == other.data && frac == other.frac;}
@@ -53,6 +54,20 @@ namespace espmath{
     void operator-=(FixedPoint& other){assert(frac == other.frac); data -= other.data;}
     void operator*=(FixedPoint& other){assert(frac == other.frac); data = FP_MUL(data, other.data, frac);}
     void operator/=(FixedPoint& other){assert(frac == other.frac); data = FP_DIV(data, other.data, frac);}
+
+    static float toFloat(int16_t value)
+    {
+      float f;
+      dsps_s16_f32_esp(&value, &f, 1);
+      return f;
+    }
+
+    static int16_t toFixed(float value)
+    {
+      int16_t f;
+      dsps_f32_s16_esp(&value, &f, 1);
+      return f;
+    }
   }fixed;
 
   fixed operator+(fixed f1, fixed f2);
